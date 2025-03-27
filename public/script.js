a4258eb67ccf48f0f3a300fb8cbdc9eec2288cb0
Bryceton <13bth13@gmail.com>
2025-03-27 19:46:48 -0400

    Got camera working but multiple not yet

--- public/script.js
+++ public/script.js
@@ -1,12 +1,54 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 const userId = String(Math.floor(Math.random() * 100000))
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true
 
-socket.emit('join-room', ROOM_ID, userId)
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
 
-socket.on('user-connected', userId => {
-    console.log('User connected:', userId)
+    myPeer.on('call', call => {
+        call.answer(stream)
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
 })
 
-socket.on('user-disconnected', userId => {
-    console.log('User disconnected:', userId)
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
 })
+
+//socket.on('user-connected', userId => {
+//    console.log('User connected:', userId)
+//})
+
+//socket.on('user-disconnected', userId => {
+//    console.log('User disconnected:', userId)
+//})
+
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video') 
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}})
